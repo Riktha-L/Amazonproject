e30b5c37663271936837aa767f6c28e4ebbd0328
Riktha-L <Riktha.l2022ai-ml@sece.ac.in>
2025-02-07 11:08:26 +0530

    Modules done

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
 import{cart} from '../data/cart.js';
+import { products } from '../data/products.js';
 let productsHTML=''; // Start with an empty string to store all product HTML.
 
 products.forEach((product) => {  // For each product in the products array:
