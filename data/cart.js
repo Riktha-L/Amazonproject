11775e338cf05d2c31beaa95161a97e57594e73e
Riktha-L <Riktha.l2022ai-ml@sece.ac.in>
2025-02-09 11:30:08 +0530

    Radio Selector

--- data/cart.js
+++ data/cart.js
@@ -2,6 +2,12 @@ export const cart=[
   {
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:1
+  },{
+    productId:"bc2847e9-5323-403f-b7cf-57fde044a955",
+    quantity:2
+  },{
+    productId:"aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
+    quantity:3
   }
 ];
 export function addtocart(productId, selectedQuantity){
